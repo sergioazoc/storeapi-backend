283a90e5ff95c4ce88552a3a945c8fb2b51da05e
Sergio Azocar <sergioazoc@gmail.com>
2020-08-13 16:44:13 -0400

    fix db

--- src/database.js
+++ src/database.js
@@ -1,20 +1,12 @@
-  const mysql = require('mysql');
+const mysql = require('mysql');
 
-const mysqlConnection = mysql.createConnection({
+const config = {
   host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
   user: 'bsale_test',
   password: 'bsale_test',
   database: 'bsale_test',
-  multipleStatements: true
-});
+};
 
-mysqlConnection.connect(function (err) {
-  if (err) {
-    console.error(err);
-    return;
-  } else {
-    console.log('db is connected');
-  }
-});
+const pool = mysql.createPool(config);
 
-module.exports = mysqlConnection;
+module.exports = pool;
--- src/routes/pruducts.js
+++ src/routes/pruducts.js
@@ -1,31 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+const pool = require('../database.js');
 
-// GET all products
 router.get('/', (req, res) => {
-  mysqlConnection.query('SELECT * FROM product', (err, rows) => {
-    if(!err) {
-      res.json(rows);
-    } else {
-      console.log(err);
-    }
-  });  
-});
+  pool.query('SELECT * FROM product', (error, result) => {
+      if (error) throw error;
 
-// GET product by id
-router.get('/:id', (req, res) => {
-  const { id } = req.params; 
-  mysqlConnection.query('SELECT * FROM product WHERE id = ?', [id], (err, rows) => {
-    if (!err) {
-      res.json(rows[0]);
-    } else {
-      res.json(err);
-      console.log(err);
-    }
+      res.send(result);
   });
 });
-
 
 module.exports = router;
