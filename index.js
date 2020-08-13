f8dfda8620e178e56085ae618cb48d365321fbf2
Sergio Azocar <sergioazoc@gmail.com>
2020-08-13 14:40:38 -0400

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // Settings
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
