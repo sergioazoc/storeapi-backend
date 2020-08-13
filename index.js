ad0b79a3a0bb45d89e126cdf5605d4a4fe03ff08
Sergio Azocar <sergioazoc@gmail.com>
2020-08-13 14:36:33 -0400

    refactor

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-app.use(require('./routes/pruducts'));
+app.use(require('./src/routes/pruducts'));
 
 // Starting the server
 app.listen(PORT, () => {
